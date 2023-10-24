7885d85a8c4597cdeedcd4e97ce4f3f34d4bb111
Jay3777 <jayaprakashbakarigalla@gmail.com>
2023-10-25 00:18:16 +0530

    initial

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>suddakuntivinayxxjugyutb tuu utu6tgujygjygftu6fy </h1>
+    </div>
+  );
+}
+
+export default App;
